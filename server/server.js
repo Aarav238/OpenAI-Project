2ac7b29fad10b06999e5d7fed3de40e863f047b2
Aarav238 <aarav8090shukla@gmail.com>
2023-01-18 21:08:17 +0530

    error fixed

--- server/server.js
+++ server/server.js
@@ -1,48 +1,47 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from 'cors';
-import { Configuration, OpenAIApi } from "openai";
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const app  = express();
-app.use(cors());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use(express.json());
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
-app.get('/' ,async (req,res) => {
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      })
     res.status(200).send({
-        message: 'Hello from Arc',
-    })
-});
+      bot: response.data.choices[0].text
+    });
 
-app.post('/' , async(req,res) => {
-    try {
-        const prompt = req.body.prompt;
-
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt:`${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-          });
-          res.status(200).send({
-            bot: response.data.choices[0].text
-          })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({error})
-        
-    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
 })
 
 app.listen(5000, 
